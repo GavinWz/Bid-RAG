6390624f0a2abaf7648c43a4ee5db59a91366059
狸猫港 <305687049@qq.com>
2024-12-25 15:09:08 +0800

    first

--- llm/test.py
+++ llm/test.py
@@ -1,3 +1,28 @@
-#模型下载
-from modelscope import snapshot_download
-model_dir = snapshot_download('Qwen/Qwen2.5-7B-Instruct-GGUF')
+# 1.安装huggingface_hub
+# pip install huggingface_hub
+
+
+import os
+
+from huggingface_hub import snapshot_download
+
+print('downloading entire files...')
+# 注意，这种方式仍然保存在cache_dir中
+snapshot_download(repo_id="ibrahimhamamci/CT-RATE", repo_type="dataset",
+                  local_dir="本地路径",
+                  local_dir_use_symlinks=False, resume_download=True,
+                  token='hf_***')
+
+# 使用cache_dir参数，将模型/数据集保存到指定“本地路径”
+snapshot_download(repo_id="ibrahimhamamci/CT-RATE", repo_type="dataset",
+                  cache_dir="本地路径",
+                  local_dir_use_symlinks=False, resume_download=True,
+                  token='hf_***')
+
+# download single file...，下载单个文件
+# from huggingface_hub import hf_hub_download
+# hf_hub_download(repo_id="ibrahimhamamci/CT-RATE", filename='config.json',
+#                 repo_type="dataset",
+#                 local_dir="/home/miao/data/dataset/CT-RATE/dataset/train",
+#                 local_dir_use_symlinks=False, resume_download=True,
+#                 force_download=False, subfolder='dataset/train/train_10006')
